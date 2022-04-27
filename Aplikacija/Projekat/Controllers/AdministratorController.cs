8d40c7b461dfcca432d4c50a1cde2fe73e9da250
LazarNajdanovic <lazarnajdanovic20@gmail.com>
2022-04-27 18:19:34 +0200

    Controller

--- Aplikacija/Projekat/Controllers/AdministratorController.cs
+++ Aplikacija/Projekat/Controllers/AdministratorController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Models;
 
 namespace SWE___PROJEKAT.Controllers
 {
@@ -12,11 +16,292 @@ namespace SWE___PROJEKAT.Controllers
     public class AdministratorController : ControllerBase
     {
 
-        public AdministratorController()
+        public ProjekatContext Context{ get; set; }
+        public AdministratorController(ProjekatContext context)
         {
-            
+            Context=context;
         }
 
+        [Route("PreuzmiAdministratora")]
+        [EnableCors("CORS")]
+        [HttpGet]
+        public async Task<ActionResult> preuzmiAdministratora()
+        {
+            try
+            {
+                var administrator = await Context.Administrator.Select(p => new{
+                            p.email,
+                            p.Password,
+                            p.telefon
+                            }).FirstOrDefaultAsync();
+                return Ok(administrator);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("DodatiKorisnika/{ime}/{prezime}/{username}/{password}/{email}")]
+        [EnableCors("CORS")]
+        [HttpPost]
+        public async Task<ActionResult> dodajKorisnik(string ime, string prezime, string username, 
+                                        string password, string email)
+        {
+            if(string.IsNullOrWhiteSpace(ime) || ime.Length > 30)
+            {
+                return BadRequest("Nevalidan unos za ime!");
+            }
+            if(string.IsNullOrWhiteSpace(prezime) || prezime.Length > 30)
+            {
+                return BadRequest("Nevalidan unos za prezime!");
+            }
+            if(string.IsNullOrWhiteSpace(username) || username.Length > 30)
+            {
+                return BadRequest("Nevalidan unos za username!");
+            }
+            if(string.IsNullOrWhiteSpace(password) || password.Length > 50)
+            {
+                return BadRequest("Nevalidan unos za password!");
+            }
+            if(string.IsNullOrWhiteSpace(email))
+            {
+                return BadRequest("Nevalidan unos za email!");
+            }
+            try
+            {
+                if(!CheckEmail(email))
+                {
+                    throw new Exception("Niste lepo uneli e-mail!");
+                }
+                var korisnik = await Context.Korisnici.Where(p => p.email == email && p.Username == username).FirstOrDefaultAsync();
+                if(korisnik != null)
+                {
+                    throw new Exception("Postoji vec korisnik sa ovim mejlom!");
+                }
+                Korisnik k = new Korisnik();
+                k.Ime = ime;
+                k.Prezime = prezime;
+                k.Username = username;
+                k.Password = password;
+                k.email = email;
+                Context.Korisnici.Add(k);
+                await Context.SaveChangesAsync();
+                return Ok("Uspesno dodat korisnik!");
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        [NonAction]
+        public bool CheckEmail(string emailaddress)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(emailaddress);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+
+        [Route("DodatiProizvodjaca/{naziv}/{username}/{password}/{email}/{telefon}/{adresa}/{datum}")]
+        [EnableCors("CORS")]
+        [HttpPost]
+        public async Task<ActionResult> dodajProizvodjaca(string naziv, string username, string password, 
+                        string email, string telefon, string adresa, DateTime datum )
+        {
+            if(string.IsNullOrWhiteSpace(naziv) || naziv.Length > 50)
+            {
+                return BadRequest("Nevalidan unos za naziv!");
+            }
+            if(string.IsNullOrWhiteSpace(username) || username.Length > 30)
+            {
+                return BadRequest("Nevalidan unos za username!");
+            }
+            if(string.IsNullOrWhiteSpace(password) || password.Length > 50)
+            {
+                return BadRequest("Nevalidan unos za password!");
+            }
+            if(string.IsNullOrWhiteSpace(email))
+            {
+                return BadRequest("Nevalidan unos za email!");
+            }
+            if(string.IsNullOrWhiteSpace(telefon))
+            {
+                return BadRequest("Nevalidan unos za telefon!");
+            }
+            if(string.IsNullOrWhiteSpace(adresa) || adresa.Length > 100)
+            {
+                return BadRequest("Nevalidan unos za adresa!");
+            }
+            try
+            {
+                if(!CheckEmail(email))
+                {
+                    throw new Exception("Niste lepo uneli e-mail!");
+                }
+                var proizvodjac = await Context.Domacinstva.Where(p => p.email == email && p.Username == username).FirstOrDefaultAsync(); 
+                if(proizvodjac != null)
+                {
+                    throw new Exception("Postoji proizvodjac!");
+                }
+                Domacinstvo d = new Domacinstvo();
+                d.Naziv = naziv;
+                d.Username = username;
+                d.Password = password;
+                d.email = email;
+                d.telefon = telefon;
+                d.Adresa = adresa;
+                d.otvorenaVrata = datum;
+                Context.Domacinstva.Add(d);
+                await Context.SaveChangesAsync();
+                return Ok("Uspesno dodat domacinstvo!");
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        } 
+
+        [Route("DodatiDostavljac/{username}/{password}/{email}/{telefon}/{cena}")]
+        [EnableCors("CORS")]
+        [HttpPost]
+        public async Task<ActionResult> dodajDostavljaca(string username, string password, string email, 
+                                    string telefon, int cena)
+        {
+            if(string.IsNullOrWhiteSpace(username) || username.Length > 30)
+            {
+                return BadRequest("Nevalidan unos za username!");
+            }
+            if(string.IsNullOrWhiteSpace(password) || password.Length > 50)
+            {
+                return BadRequest("Nevalidan unos za password!");
+            }
+            if(string.IsNullOrWhiteSpace(email))
+            {
+                return BadRequest("Nevalidan unos za email!");
+            }
+            if(string.IsNullOrWhiteSpace(telefon))
+            {
+                return BadRequest("Nevalidan unos za telefon!");
+            }
+            if(cena == 0)
+            {
+                return BadRequest("Cena ne moze da bude 0");
+            }
+            try
+            {
+                if(!CheckEmail(email))
+                {
+                    throw new Exception("Niste lepo uneli e-mail!");
+                }
+                var dostavljac = await Context.Dostavljaci.Where(p => p.email == email && p.Username == username).FirstOrDefaultAsync(); 
+                if(dostavljac != null) 
+                {
+                    throw new Exception("Postoji dostavljac!");
+                }
+                Dostavljac dos = new Dostavljac();
+                dos.Username = username;
+                dos.Password = password;
+                dos.email = email;
+                dos.telefon = telefon;
+                dos.Cena = cena;
+                Context.Dostavljaci.Add(dos);
+                await Context.SaveChangesAsync();
+                return Ok("Uspesno dodat dostavljac!");
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
         
+        [Route("ObrisatiKorisnika/{email}")]
+        [EnableCors("CORS")]
+        [HttpDelete]
+        public async Task<ActionResult> obrisiKorisnika(string email)
+        {
+            if(string.IsNullOrWhiteSpace(email))
+            {
+                return BadRequest("Nevalidan unos za email!");
+            }
+            try
+            {
+                var korisnik = await Context.Korisnici.Where(p => p.email==email).FirstOrDefaultAsync();
+                if(korisnik == null)
+                {
+                    throw new Exception("Ne postoji korisnik!");
+                }
+                var username = korisnik.Username;
+                Context.Korisnici.Remove(korisnik);
+                await Context.SaveChangesAsync();
+                return Ok($"Uspesno smo uklonili korisnika {username}");
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [Route("ObrisatiProizvodjaca/{email}")]
+        [EnableCors("CORS")]
+        [HttpDelete]
+        public async Task<ActionResult> obrisiProizvodjaca(string email)
+        {
+            if(string.IsNullOrWhiteSpace(email))
+            {
+                return BadRequest("Nevalidan unos za email!");
+            }
+            try
+            {
+                var proizvodjac = await Context.Domacinstva.Where(p => p.email==email).FirstOrDefaultAsync();
+                if(proizvodjac == null)
+                {
+                    throw new Exception("Ne postoji proizvodjac!");
+                }
+                var naziv = proizvodjac.Naziv;
+                Context.Domacinstva.Remove(proizvodjac);
+                await Context.SaveChangesAsync();
+                return Ok($"Uspesno smo uklonili domacinstvo {naziv}");
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }  
+
+        [Route("ObrisatiDostavljaca/{email}")]
+        [EnableCors("CORS")]
+        [HttpDelete]
+        public async Task<ActionResult> obrisiDostavljaca(string email)
+        {
+            if(string.IsNullOrWhiteSpace(email))
+            {
+                return BadRequest("Nevalidan unos za email!");
+            }
+            try
+            {
+                var dostavljac = await Context.Dostavljaci.Where(p => p.email==email).FirstOrDefaultAsync();
+                if(dostavljac == null)
+                {
+                    throw new Exception("Ne postoji proizvodjac!");
+                }
+                var username = dostavljac.Username;
+                Context.Dostavljaci.Remove(dostavljac);
+                await Context.SaveChangesAsync();
+                return Ok($"Uspesno smo uklonili dostavljaca {username}");
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }             
     }
 }
