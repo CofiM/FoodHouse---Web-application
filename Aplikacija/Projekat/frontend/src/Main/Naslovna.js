7757cbf029e313e66c80528907d369023c3b40c0
nikolamomcilovic6 <nikola.momcilovic@elfak.rs>
2022-05-25 14:07:58 +0200

    front

--- Aplikacija/Projekat/frontend/src/Main/Naslovna.js
+++ Aplikacija/Projekat/frontend/src/Main/Naslovna.js
@@ -1,6 +1,22 @@
-const Naslovna = () => {
+import React from 'react';
 
-    return <p>Naslovna</p>
+import Pretraga from '../Pretraga/Pretraga';
+import classes from "./Pretraga.module.css";
+
+const Naslovna=()=>
+{
+    return (
+        <div className={classes.container}>
+            <div className={classes.pretragaDiv}>
+            <div><h1>Pronađite svog omiljenog domaćina</h1></div>
+           <div> <Pretraga></Pretraga></div> 
+           <div  ></div>
+
+            </div>
+
+
+        </div>
+    );
 };
 
 export default Naslovna;
