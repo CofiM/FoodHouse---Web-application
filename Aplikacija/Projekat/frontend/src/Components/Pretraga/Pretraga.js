db5b6b19d04a9ce35561da8d09fb65209fd669ee
nikolamomcilovic6 <nikola.momcilovic@elfak.rs>
2022-05-28 19:49:47 +0200

    front

--- Aplikacija/Projekat/frontend/src/Components/Pretraga/Pretraga.js
+++ Aplikacija/Projekat/frontend/src/Components/Pretraga/Pretraga.js
@@ -1,27 +1,68 @@
 import * as ReactDOM from "react-dom";
 import classes from "./Pretraga.module.css";
 import React, { Component } from 'react';
+import  { useState } from 'react';
 import Select from 'react-select';
 
 import Button from "@mui/material/Button";
 
-
+var category;
 async function fetchProductsHandler()
 {
-  const response= await fetch('/Proizvod/PreuzetiProizvodeZaKategoriju/{kategorija}')
+   
+    const response = await fetch('https://localhost:5001/Proizvod/PreuzetiProizvodeZaKategoriju/' + category,
+        {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json;charset=UTF-8'
+            }
+        });
+    const data = await response.json();
+    
+
 };
 
 
 const Pretraga=()=>
 {
+    const [value, setValue] = useState();
+
+    const handleChange = (e) => {
+        setValue(e.target.value);
+    }
+
+
+     category = value;
     const categoryArray = [
-        { label: "Mlečni proizvodi"},
-  { label: "Med i proizvodi od meda"},
-  { label: "Rakije"  },
-  { label: "Meso i mesne prerađevine " },
-  { label: "Domaća jaja" },
-  { label: "Džem i slatko"  },
-  { label: "Voće i povrće"}
+        {
+            label: "Mlečni proizvodi",
+            value: "Mlečni proizvodi",
+        },
+        {
+            label: "Med i proizvodi od meda",
+            value: "Med i proizvodi od meda",
+        },
+        {
+            label: "Rakije",
+            value: "Rakije",
+        },
+        {
+            label: "Meso i mesne prerađevine",
+            value: "Meso i mesne prerađevinekije",
+        },
+        {
+            label: "Domaća jaja",
+            value: "Domaća jaja",
+        },
+        {
+            label: "Džem i slatko",
+            value: "Džem i slatko",
+        },
+        {
+            label: "Voće i povrće",
+            value: "Voće i povrće",
+            
+        }
       ];
     
       const dos=[
@@ -36,14 +77,14 @@ const Pretraga=()=>
         <input type='text' placeholder="Koji proizvod želite da pronađete" ></input>
         <div className="example-config">
       </div>
-      <div>
-        <Select options={categoryArray}
-        placeholder="Kategorije"
-          style={{
-            width: "300px",
-          }}
-          
-        />
+            <div>
+                <select className="Category" onChange={handleChange} >
+                    {categoryArray.map((option) => (
+                        <option value={option.value}>{option.label}</option>
+                    ))}
+                </select>
+       
+        
       </div>
       <div>
       <Select options={dos}
@@ -52,8 +93,8 @@ const Pretraga=()=>
           width: "300px",
         }}
       />
-      </div>
-      <Button variant="contained" color="success">
+            </div>
+            <Button variant="contained" color="success" onClick={fetchProductsHandler} >
        Pretraga
       </Button>
 
