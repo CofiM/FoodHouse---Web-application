32b45ba9f7977ecd4da92d046765a1492d78caa6
LazarNajdanovic <lazarnajdanovic20@gmail.com>
2022-06-02 22:39:39 +0200

    front

--- Aplikacija/Projekat/frontend/src/Components/Poslovi/ViewJobsDate.js
+++ Aplikacija/Projekat/frontend/src/Components/Poslovi/ViewJobsDate.js
@@ -10,7 +10,7 @@ const ViewJobsDate=()=>{
 
     const date = localStorage.getItem("Date");
 
-        useEffect(() => {
+    useEffect(() => {
         async function searchWithData() {
             const response = await fetch('https://localhost:5001/Posao/PreuzetiPosloveZaDatum/'+ date,
             {
@@ -22,8 +22,9 @@ const ViewJobsDate=()=>{
             
             const data = await response.json();
             console.log(data);
-            const jobs= data.map((job)=>{
+            const jobs= data.map((job, index)=>{
             return{
+                indeks: index ,
                 opis: job.opis,
                 brojRadnihMesta: job.brojRadnihMesta,
                 datumPosla: job.datum,
@@ -38,31 +39,37 @@ const ViewJobsDate=()=>{
             console.log(jobs);
             };
             searchWithData();
-        }, []);
+    }, []);
 
-        return (
-            <div className={classes.search}>
-         
-                {/* <div className={classes.searchDiv}>
-                    <input type="text" placeholder="Lokacija" onChange={adresaHandler} />
-                    <input type="date" value="2022-05-15" min="2022-01-01" max="2022-12-31" />
-                    <button onClick={searchWithLocation} >Pretrazi</button>
-                </div> */}
-                <div>
-                    { allJobs.map((job) => (
-                       <PosloviCard
-                            opis = {job.opis}
-                            brRadnihMesta = {job.brojRadnihMesta}
-                            datum = {job.datumPosla}
-                            cena = {job.cena}
-                            domacin = {job.domacin}
-                            adresa = {job.adresa}
-                        />
-                        ))}
-                </div>
-            
-        </div>
-        );
+    const onClickPosaoCard = () => {
+        console.log("ULAZIM!");
+        //console.log(index);
+    }
+
+    return (
+        <div className={classes.search}>
+        
+            {/* <div className={classes.searchDiv}>
+                <input type="text" placeholder="Lokacija" onChange={adresaHandler} />
+                <input type="date" value="2022-05-15" min="2022-01-01" max="2022-12-31" />
+                <button onClick={searchWithLocation} >Pretrazi</button>
+            </div> */}
+            <div>
+                { allJobs.map((job) => (
+                    <PosloviCard
+                        opis = {job.opis}
+                        brRadnihMesta = {job.brojRadnihMesta}
+                        datum = {job.datumPosla}
+                        cena = {job.cena}
+                        domacin = {job.domacin}
+                        adresa = {job.adresa}
+                        klik = { onClickPosaoCard }
+                    />
+                    ))}
+            </div>
+        
+    </div>
+    );
 
 
 };
