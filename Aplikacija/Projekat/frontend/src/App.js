a24384a2cdf1a11deea4306774c10654d001d8b3
LazarNajdanovic <lazarnajdanovic20@gmail.com>
2022-06-07 23:07:13 +0200

    Front

--- Aplikacija/Projekat/frontend/src/App.js
+++ Aplikacija/Projekat/frontend/src/App.js
@@ -33,6 +33,7 @@ import ProfilKorisnik from "./Components/Profil/ProfileKorisnik";
 import HouseholdView from "./Components/Domacinstvo/HouseholdView";
 import ViewJobsDate from "./Components/Poslovi/ViewJobsDate";
 import ViewJobsDateLocation from "./Components/Poslovi/ViewJobsDateLocation";
+import ViewJobsLocation from "./Components/Poslovi/ViewJobsLocation";
 
 
 
