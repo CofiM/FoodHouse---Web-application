4113655ac412849879f98dde1432705bdbfbaa05
LazarNajdanovic <lazarnajdanovic20@gmail.com>
2022-06-09 01:24:33 +0200

    Front

--- Aplikacija/Projekat/frontend/src/Main/NarudzbinaCard.js
+++ Aplikacija/Projekat/frontend/src/Main/NarudzbinaCard.js
@@ -4,61 +4,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import classes from "./Narudzbina.module.css";
-
-
-/* const card = (
-  <div className={classes.mainPart}>
-    <CardContent>
-      <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-        Naručilac:
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        Lazar
-      </Typography>
-    </CardContent>
-    <CardContent>
-      <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-        Adresa:
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        Milovana GLisica
-      </Typography>
-    </CardContent>
-    <CardContent>
-      <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-        Proizvod:
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        Maline
-      </Typography>
-    </CardContent>
-    <CardContent>
-      <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-        Količina:
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        2
-      </Typography>
-    </CardContent>
-    <CardContent>
-      <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-        Domaćinstvo:
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        Gazdinstvo Maletić
-      </Typography>
-    </CardContent>
-    <CardContent>
-      <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-        Adresa:
-      </Typography>
-      <Typography sx={{ mb: 1.5 }} color="text.secondary">
-        Golemog 9A
-      </Typography>
-    </CardContent>
-
-  </div>
-); */
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
 
 export default function OutlinedCard(props) {
   return (
@@ -66,54 +12,59 @@ export default function OutlinedCard(props) {
       <Card variant="outlined">
           {
               <div className={classes.mainPart}>
-              <CardContent>
-                <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-                  Naručilac:
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {props.ime + " " + props.prezime}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-                  Adresa:
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {props.adresaKorisnika}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-                  Proizvod:
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {props.nazivProizvoda}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-                  Količina:
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {props.kolicinaProizvoda}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-                  Domaćinstvo:
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {props.domacinstvoNaziv}
-                </Typography>
-              </CardContent>
-              <CardContent>
-                <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-                  Adresa:
-                </Typography>
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                  {props.domacinstvoAdresa}
-                </Typography>
-              </CardContent>
+                <div className={classes.mainPart}>
+                  <CardContent>
+                    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                      Naručilac:
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {props.ime + " " + props.prezime}
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                      Adresa:
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {props.adresaKorisnika}
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                      Proizvod:
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {props.nazivProizvoda}
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                      Količina:
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {props.kolicinaProizvoda}
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                      Domaćinstvo:
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {props.domacinstvoNaziv}
+                    </Typography>
+                  </CardContent>
+                  <CardContent>
+                    <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
+                      Adresa:
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {props.domacinstvoAdresa}
+                    </Typography>
+                  </CardContent>
+                </div>
+                <div>
+                    <DeleteForeverRoundedIcon />
+                </div>
             </div>
         }
       </Card>
