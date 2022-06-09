57a0079d16ed6bc47dedce3070e86afba793c215
jovanmalke <jovan.maletic@elfak.rs>
2022-06-10 00:27:27 +0200

    dizajn

--- Aplikacija/Projekat/frontend/src/Components/Proizvod/ImageGallery.js
+++ Aplikacija/Projekat/frontend/src/Components/Proizvod/ImageGallery.js
@@ -1,55 +1,50 @@
-import { useEffect, useState } from 'react';
-import classes from './ImageGallery.module.css';
+import { useEffect, useState } from "react";
+import classes from "./ImageGallery.module.css";
 
+const ImageGallery = (props) => {
+  const [selectedImg, setSelectedImg] = useState(null);
+  const [imageArray, setImageArray] = useState(null);
+  const [show, setShow] = useState(false);
 
-const ImageGallery = (props) =>{
- 
-    const [selectedImg,setSelectedImg]=useState(null);
-    const [imageArray,setImageArray] = useState(null);
-    const [show,setShow]=useState(false);
-
-
-
-
-
-    useEffect(()=>{
-        async function fetchData() {
-            const response = await fetch('https://localhost:5001/FileUpload/'+2);
-            const data = await response.json();
-            console.log(data);
-            setImageArray(data);
-            setShow(true);
-            data.map(item => setSelectedImg(item));  
-        }
-        fetchData();
-        console.log(selectedImg);
-        console.log(imageArray);
-        console.log(show);
-    },[]);
-    if(show==true)
-    {
-    return(
-
-        <div className={classes.container}>
-            <img src = {"data:image/png;base64,"+selectedImg} alt='Selected' className={classes.selected}/> 
-            <div className={classes.imgContainer}>
-                {show && imageArray.map((img,index)=>(
-                    
-                    <img
-                        className={classes.image}
-                        style={{border:selectedImg === img ? '4px solid blue' : ''}}
-                        key={index}
-                        src={"data:image/png;base64,"+img}
-                        onClick={()=>setSelectedImg(img)}
-                    />
-                )
-                )}
-            </div>      
-            
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://localhost:5001/FileUpload/" + 2);
+      const data = await response.json();
+      console.log(data);
+      setImageArray(data);
+      setShow(true);
+      data.map((item) => setSelectedImg(item));
+    }
+    fetchData();
+    console.log(selectedImg);
+    console.log(imageArray);
+    console.log(show);
+  }, []);
+  if (show == true) {
+    return (
+      <div className={classes.container}>
+        <img
+          src={"data:image/png;base64," + selectedImg}
+          alt="Selected"
+          className={classes.selected}
+        />
+        <div className={classes.imgContainer}>
+          {show &&
+            imageArray.map((img, index) => (
+              <img
+                className={classes.image}
+                style={{ border: selectedImg === img ? "4px solid blue" : "" }}
+                key={index}
+                src={"data:image/png;base64," + img}
+                onClick={() => setSelectedImg(img)}
+              />
+            ))}
         </div>
+      </div>
     );
-                }
-                else{<p>loading...</p>}
-}
+  } else {
+    <p>loading...</p>;
+  }
+};
 
 export default ImageGallery;
--- Aplikacija/Projekat/frontend/src/Components/Proizvod/Proizvod.js
+++ Aplikacija/Projekat/frontend/src/Components/Proizvod/Proizvod.js
@@ -1,14 +1,11 @@
-import React,{ useRef,useState,useEffect} from 'react';
-import { useLocation } from 'react-router-dom';
-import classes from './Proizvod.module.css';
-import ImageGallery from './ImageGallery';
+import React, { useRef, useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import classes from "./Proizvod.module.css";
+import ImageGallery from "./ImageGallery";
 import { useCart } from "react-use-cart";
-import CheckBox from '../Proizvod/CheckBox';
-
-
+import CheckBox from "../Proizvod/CheckBox";
 
 const Proizvod = (props) => {
-  
   const cenaDostave = JSON.parse(localStorage.getItem("CenaDostave"));
   const idDomacinstva = JSON.parse(localStorage.getItem("DomacinstvoID"));
   const idDostavljaca = JSON.parse(localStorage.getItem("idDostavljaca"));
@@ -16,21 +13,22 @@ const Proizvod = (props) => {
   const imeDomacinstva = JSON.parse(localStorage.getItem("naz"));
   let type = localStorage.getItem("Korisnik");
 
-  const [show,setShow]=useState(false);
+  const [show, setShow] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     async function fetchData() {
-        const response = await fetch('https://localhost:5001/Domacinstvo/VratiDostavljacaZaDomacinstvo/'+idDomacinstva);
-        const data = await response.json();
-        console.log(data);
-        if(data.dostavljac!==null)
-        {
-          setShow(true);
-        }
+      const response = await fetch(
+        "https://localhost:5001/Domacinstvo/VratiDostavljacaZaDomacinstvo/" +
+          idDomacinstva
+      );
+      const data = await response.json();
+      console.log(data);
+      if (data.dostavljac !== null) {
+        setShow(true);
+      }
     }
     fetchData();
-    },[]);
-
+  }, []);
 
   const [checked, setChecked] = React.useState(false);
 
@@ -38,118 +36,136 @@ const Proizvod = (props) => {
     setChecked(!checked);
   };
 
-  
-  const [Data,setData]=useState([]);
+  const [Data, setData] = useState([]);
 
-  const amountInputRef=useRef();
+  const amountInputRef = useRef();
 
   const location = useLocation();
 
-  const{inCart,addItem,updateItemQuantity,getItem,cartTotal}=useCart();
+  const { inCart, addItem, updateItemQuantity, getItem, cartTotal } = useCart();
 
   var obj;
 
   const [isLoading, setIsLoading] = useState(false);
 
-   async function fetchAddNewDelivery(dal,count)
-   {
-     let temp = 0;
-     if(dal === 0)
-     {
-        temp = 0;
-     }
-     else
-     {
-        temp = idDostavljaca;
-     }
-      console.log(dal);
-     const response = await fetch('https://localhost:5001/Narudzbine/DodatiNarudzbinu/'+idKorisnika+'/'+proba.id+'/'+idDomacinstva+'/'+temp+'/'+cenaDostave+'/'+dal+'/'+count+'/'+proba.price+'/'+proba.name,{
-       method:'POST',
-       body:JSON.stringify({title:'Uspesno dodatno'}),
-       headers:{
-         'Content-Type':'application/json'
-       }
-     });
+  async function fetchAddNewDelivery(dal, count) {
+    let temp = 0;
+    if (dal === 0) {
+      temp = 0;
+    } else {
+      temp = idDostavljaca;
+    }
+    console.log(dal);
+    const response = await fetch(
+      "https://localhost:5001/Narudzbine/DodatiNarudzbinu/" +
+        idKorisnika +
+        "/" +
+        proba.id +
+        "/" +
+        idDomacinstva +
+        "/" +
+        temp +
+        "/" +
+        cenaDostave +
+        "/" +
+        dal +
+        "/" +
+        count +
+        "/" +
+        proba.price +
+        "/" +
+        proba.name,
+      {
+        method: "POST",
+        body: JSON.stringify({ title: "Uspesno dodatno" }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-     const data = await response.json();
-     console.log(data);
- 
-   }
+    const data = await response.json();
+    console.log(data);
+  }
 
-  
-  console.log(location.product)
-
- 
+  console.log(location.product);
 
   let item = JSON.parse(JSON.stringify(location.product));
   let proba = {
-    id:item.ID,
-    name:item.Naziv + " - " + imeDomacinstva,
-    price:item.Cena,
-    quantity:item.Kolicina,
-  }
+    id: item.ID,
+    name: item.Naziv + " - " + imeDomacinstva,
+    price: item.Cena,
+    quantity: item.Kolicina,
+  };
 
-  const checkItem = ()=> {
+  const checkItem = () => {
     let check = inCart(proba.id);
     let dostavaCheck = 0;
     let count;
     console.log(checked);
-    if(check === true)
-    {
+    if (check === true) {
       let temp = getItem(proba.id);
       console.log(temp);
       let br1 = +amountInputRef.current.value;
       let br2 = +temp.quantity;
-      let sab = br1+br2;
-      updateItemQuantity(proba.id,sab.toString());
-      count=sab;
+      let sab = br1 + br2;
+      updateItemQuantity(proba.id, sab.toString());
+      count = sab;
+    } else {
+      addItem(proba, amountInputRef.current.value);
+      count = amountInputRef.current.value;
     }
-    else
-    {
-      addItem(proba,amountInputRef.current.value);
-      count=amountInputRef.current.value;
-    }
-    if(checked===true)
-    {
-      dostavaCheck=1;
+    if (checked === true) {
+      dostavaCheck = 1;
     }
     console.log(count);
     console.log(dostavaCheck);
     console.log(proba.name);
-    fetchAddNewDelivery(dostavaCheck,count);
-  }
+    fetchAddNewDelivery(dostavaCheck, count);
+  };
 
-          if(!isLoading){return(<div  className = {classes['card-wrapper']}>
-            <ImageGallery  IdSlike={proba.id}/>
-        
-          <div className = {classes['product-content']}>
-            <h2 classname = {classes['product-title']}>{location.product.Naziv}</h2>
-            <p>Zavisno od proizvoda,cena proizvoda je po komadu ili kilogramu</p>
-  
-            <div className = {classes['product-price']}>
-              <p className = 'new-price'>Cena: <span>{location.product.Cena}.00 din</span></p>
-            </div>
-  
-            <div className = {classes['product-detail']}>
-              <h2>O proizvodu: </h2>
-              <p>{location.product.Opis}</p>
-            </div>
-             {show && <p>Usluga dostave : {cenaDostave}.00 din</p> }
-            {show &&<CheckBox
-                label="Dostava"
-                value={checked}
-                onChange={handleChange}
-            />}
-              <div className = {classes['purchase-info']}>
-              <input ref={amountInputRef} type='number' min='1' max='100' step='1' defaultValue='1'/>
-              <button onClick={checkItem} type = 'button' className = {classes.btn}>
-                Dodaj u korpu
-              </button>
-            </div>       
+  if (!isLoading) {
+    return (
+      <div className={classes["card-wrapper"]}>
+        <ImageGallery IdSlike={proba.id} />
+
+        <div className={classes["product-content"]}>
+          <h2 classname={classes["product-title"]}>{location.product.Naziv}</h2>
+          <p>Zavisno od proizvoda,cena proizvoda je po komadu ili kilogramu</p>
+
+          <div className={classes["product-price"]}>
+            <p className="new-price">
+              Cena: <span>{location.product.Cena}.00 din</span>
+            </p>
           </div>
-      </div>);}
-      else{return(<p>loading..</p>);}
-  
+
+          <div className={classes["product-detail"]}>
+            <h2>O proizvodu: </h2>
+            <p>{location.product.Opis}</p>
+          </div>
+          {show && <p>Usluga dostave : {cenaDostave}.00 din</p>}
+          {show && (
+            <CheckBox label="Dostava" value={checked} onChange={handleChange} />
+          )}
+          <div className={classes["purchase-info"]}>
+            <input
+              ref={amountInputRef}
+              type="number"
+              min="1"
+              max="100"
+              step="1"
+              defaultValue="1"
+            />
+            <button onClick={checkItem} type="button" className={classes.btn}>
+              Dodaj u korpu
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <p>loading..</p>;
+  }
 };
 
 export default Proizvod;
